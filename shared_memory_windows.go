6c96ef1595f301d8affddbb11d3c77a8dedf0030
Aleksandr Demakin <alexander.demakin@gmail.com>
2015-12-04 00:48:39 +0300

    shm: windows files

--- shared_memory_windows.go
+++ shared_memory_windows.go
@@ -2,14 +2,41 @@
 
 package ipc
 
-import "os"
+import (
+	"os"
+)
 
 type memoryObjectImpl struct {
 	file *os.File
 }
 
+// Shared memory on Windows is emulated via usual files
+// like it is done in boost c++ library
 type memoryRegionImpl struct {
 	data       []byte
 	size       int
-	pageOffset int64
+}
+
+func newMemoryObjectImpl(name string, mode int, perm os.FileMode) (impl *memoryObjectImpl, err error) {
+	fullName, err := shmName(name)
+	if err != nil {
+		return nil, err
+	}
+}
+
+func shmName(name string) (string, error) {
+	if path, err  := sharedDirName(); err != nil {
+		return "", err
+	} else {
+		return path + "/" + name, nil
+	}
+}
+
+func sharedDirName() (string,  error) {
+	rootPath :=  os.TempDir() + "/go-ipc"
+	if err := os.Mkdir(rootPath, 0644); err == nil || os.IsExist(err) {
+		return rootPath, nil
+	} else {
+		return "", err
+	}
 }
