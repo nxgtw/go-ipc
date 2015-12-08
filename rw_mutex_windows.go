78749b68c1c648eeb7a0ebde6848c91d8241b19c
Aleksandr Demakin <alexander.demakin@gmail.com>
2015-12-09 00:25:34 +0300

    windows: shared memory and add rw_mutex stub

---
+++ rw_mutex_windows.go
@@ -0,0 +1,7 @@
+// Copyright 2015 Aleksandr Demakin. All rights reserved.
+
+package ipc
+
+type rwMutexImpl struct {
+	
+}
--- shared_memory_windows.go
+++ shared_memory_windows.go
@@ -3,7 +3,11 @@
 package ipc
 
 import (
+	"path/filepath"
 	"os"
+	"fmt"
+	
+	"golang.org/x/sys/windows"
 )
 
 type memoryObjectImpl struct {
@@ -18,10 +22,91 @@ type memoryRegionImpl struct {
 }
 
 func newMemoryObjectImpl(name string, mode int, perm os.FileMode) (impl *memoryObjectImpl, err error) {
-	fullName, err := shmName(name)
+	path,  err := shmName(name)
 	if err != nil {
 		return nil, err
 	}
+	osMode, err := modeToOsMode(mode)
+	if err != nil {
+		return nil, err
+	}
+	file, err := os.OpenFile(path, osMode, perm)
+	if err != nil {
+		return nil, err
+	}
+	return &memoryObjectImpl{file},  nil
+}
+
+func (impl *memoryObjectImpl) Destroy() error {
+	if err := impl.Close(); err == nil {
+		return os.Remove(impl.file.Name())
+	} else {
+		return err
+	}
+}
+
+func (impl *memoryObjectImpl) Name() string {
+	return filepath.Base(impl.file.Name())
+}
+
+func (impl *memoryObjectImpl) Close() error {
+	return impl.file.Close()
+}
+
+func (impl *memoryObjectImpl) Truncate(size int64) error {
+	return impl.file.Truncate(size)
+}
+
+func (impl *memoryObjectImpl) Size() int64 {
+	if fileInfo, err := impl.file.Stat(); err != nil {
+		return 0
+	} else {
+		return fileInfo.Size()
+	}
+}
+
+func (impl *memoryObjectImpl) Fd() int {
+	return int(impl.file.Fd())
+}
+
+func newMemoryRegionImpl(obj MappableHandle, mode int, offset int64, size int) (*memoryRegionImpl, error) {
+	prot, flags, err := shmProtAndFlagsFromMode(mode)
+	if err != nil {
+		return nil, err
+	}
+	//file:///home/avd/dev/boost_1_59_0/boost/interprocess/mapped_region.hpp:441
+	// TODO(avd) - check if it is not for shm
+	handle := windows.InvalidHandle
+	if true {
+		// TODO(avd) - security attrrs
+		var err error
+		if handle, err = windows.CreateFileMapping(windows.Handle(obj.Fd()), nil, prot , 0, 0, nil); err != nil {
+			return nil, err
+		}
+	} else {
+		
+		// TODO(avd) - finish with it
+	}
+	return nil, nil
+}
+
+func shmProtAndFlagsFromMode(mode int) (prot uint32, flags int, err error) {
+	switch mode {
+	case SHM_READ_ONLY:
+		fallthrough
+	case SHM_READ_PRIVATE:
+		prot = windows.PAGE_READONLY
+		flags = windows.FILE_MAP_READ
+	case SHM_READWRITE:
+		prot = windows.PAGE_READWRITE
+		flags = windows.FILE_MAP_WRITE
+	case SHM_COPY_ON_WRITE:
+		prot = windows.PAGE_WRITECOPY
+		flags = windows.FILE_MAP_COPY
+	default:
+		err = fmt.Errorf("invalid shm region flags")
+	}
+	return
 }
 
 func shmName(name string) (string, error) {
