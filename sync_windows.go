e084f43fe5f987ca24dd307dee2ee192c0dd38ed
Aleksandr Demakin <alexander.demakin@gmail.com>
2016-03-02 00:35:29 +0300

    sync/windows: windows syscalls for mutex

--- sync_windows.go
+++ sync_windows.go
@@ -2,7 +2,13 @@
 
 package ipc
 
-import "syscall"
+import (
+	"os"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
 
 var (
 	modkernel32      = syscall.NewLazyDLL("kernel32.dll")
@@ -10,3 +16,35 @@ var (
 	procOpenMutex    = modkernel32.NewProc("OpenMutexW")
 	procReleaseMutex = modkernel32.NewProc("ReleaseMutex")
 )
+
+func createMutex(name string) (windows.Handle, error) {
+	namep, err := windows.UTF16PtrFromString(name)
+	if err != nil {
+		return windows.InvalidHandle, err
+	}
+	h, _, err := procCreateMutex.Call(0, 0, uintptr(unsafe.Pointer(namep)))
+	if h == 0 {
+		return windows.InvalidHandle, os.NewSyscallError("CreateMutex", err)
+	}
+	return windows.Handle(h), nil
+}
+
+func openMutex(name string) (windows.Handle, error) {
+	namep, err := windows.UTF16PtrFromString(name)
+	if err != nil {
+		return windows.InvalidHandle, err
+	}
+	h, _, err := procOpenMutex.Call(uintptr(windows.SYNCHRONIZE), 0, uintptr(unsafe.Pointer(namep)))
+	if h == 0 {
+		return windows.InvalidHandle, os.NewSyscallError("OpenMutex", err)
+	}
+	return windows.Handle(h), nil
+}
+
+func releaseMutex(handle windows.Handle) error {
+	result, _, err := procReleaseMutex.Call(uintptr(handle))
+	if result == 0 {
+		return os.NewSyscallError("ReleaseMutex", err)
+	}
+	return nil
+}
