4ed502ec6b5a25c561193075442daf5ce4948be0
Aleksandr Demakin <alexander.demakin@gmail.com>
2016-03-02 00:34:53 +0300

    sync: IPCLocker interface for all sync objects

--- sync_common.go
+++ sync_common.go
@@ -2,6 +2,24 @@
 
 package ipc
 
+import (
+	"sync"
+)
+
+// this is to ensure, that all implementations of ipc mutex
+// satisfy the same minimal interface
+var (
+	_ IPCLocker = (*SpinMutex)(nil)
+	_ IPCLocker = (*Mutex)(nil)
+)
+
+// IPCLocker is a minimal interface, which must be satisfied by any synchronization primitive
+// on any platform
+type IPCLocker interface {
+	sync.Locker
+	Finish() error
+}
+
 func checkMutexOpenMode(mode int) bool {
 	return mode == O_OPEN_OR_CREATE || mode == O_CREATE_ONLY || mode == O_OPEN_ONLY
 }
