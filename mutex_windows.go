d3b4db85b09773a58e123d897fcb5f6329dafb27
Aleksandr Demakin <alexander.demakin@gmail.com>
2016-03-02 00:36:51 +0300

    sync/mutex: windows implementation

--- mutex_windows.go
+++ mutex_windows.go
@@ -12,11 +12,30 @@ type mutexImpl struct {
 	handle windows.Handle
 }
 
-func newMutexImpl(name string, mode int, perm os.FileMode) (impl *mutexImpl, resultErr error) {
+func newMutexImpl(name string, mode int, perm os.FileMode) (*mutexImpl, error) {
+	var handle windows.Handle
+	var err error
 	switch mode {
 	case O_OPEN_ONLY:
+
 	case O_CREATE_ONLY:
+		handle, err = createMutex(name)
 	case O_OPEN_OR_CREATE:
 	}
-	panic("unimplemented")
+	if err != nil {
+		return nil, err
+	}
+	return &mutexImpl{handle: handle}, nil
+}
+
+func (m *mutexImpl) Lock() {
+	windows.WaitForSingleObject(m.handle, windows.INFINITE)
+}
+
+func (m *mutexImpl) Unlock() {
+	releaseMutex(m.handle)
+}
+
+func (m *mutexImpl) Finish() error {
+	return windows.CloseHandle(m.handle)
 }
